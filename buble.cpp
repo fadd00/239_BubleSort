5e8d8f16140cab282a0edab390b8bc83a43d81e3
fadd00 <andhikahutama9@gmail.com>
2024-03-07 15:54:52 +0700

    membuat display

--- buble.cpp
+++ buble.cpp
@@ -27,8 +27,24 @@ void input(){
         cin >> arr[i];
     }
     
-} void bubbleSortArray(){
-    int pass= 1;
+} void bubbleSortArray(){ //procedur untuk mengurutkan array
+    int pass= 1; //step 1
+    do {
+        for(int j = 0; j <= n - 1 - pass; j++){ //step 2
+            if(arr[j] > arr[j + 1]){ //step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1; //step 4 
+    } while (pass < n - 1); //step 5
+} void display (){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    for
 }
 int main()
 {
