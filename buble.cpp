e302a25e4fda19a2c75106d745ae72238cc28ed1
fadd00 <andhikahutama9@gmail.com>
2024-03-07 15:59:51 +0700

    memanggil semua variable

--- buble.cpp
+++ buble.cpp
@@ -44,9 +44,17 @@ void input(){
     cout << "=================================" << endl;
     cout << "Element Array yang telah tersusun" << endl;
     cout << "=================================" << endl;
-    for
+    for( int j = 0; j < n; j++){
+        cout << arr[j] << endl; //output setiap element array pada garis baru
+    }
+    cout << "jumlah pass = " << n-1  << endl;
+    cout << endl;  
 }
+
 int main()
 {
-    cout <<"hello world!\n";
+        input();
+        bubbleSortArray();
+        display();
+        system("pause");
 }
